51ba0da1af44faadae1ae3c43f5247b18172abfb
Patrick Oliver <poliver24@hotmail.com>
2020-05-04 17:53:19 +0100

    API saves to CSV file, CSV file writes to Database

--- API/data_handler.py
+++ API/data_handler.py
@@ -1,10 +1,33 @@
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
 import csv
+import psycopg2
 
-col_list = ["TLE_LINE1", "TLE_LINE2"]
-TLE_data = pd.read_csv("api_data.csv", usecols=col_list)
+# col_list = ["TLE_LINE1", "TLE_LINE2"]
+# TLE_data = pd.read_csv("api_data.csv", usecols=col_list)
 
-TLE_data.to_csv('TLE_data.csv')
+# TLE_data.to_csv('TLE_data.csv')
 
-print('saved TLE_Data')
+# print('saved TLE_Data')
+
+
+# conn = psycopg2.connect(host="127.0.0.1",
+#                                 port="5432",
+#                                 database="sat_data")
+# cur= conn.cursor()
+
+# f = open(r'api_data.csv', 'r')
+# cur.copy_from(f, 'sat_data', sep=',')
+# f.close()
+
+# conn.commit()
+
+conn = psycopg2.connect("host=localhost dbname=sat_data")
+cur = conn.cursor()
+with open('api_data.csv', 'r') as f:
+    # Notice that we don't need the `csv` module.
+    next(f) # Skip the header row.
+    cur.copy_from(f, 'orbits', sep=',')
+
+conn.commit()
+ 
