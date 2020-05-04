70caf97752d119983c26dec0a5759a98a6fe8aab
Anton Plisnier <anttongp@gmail.com>
2020-05-04 15:54:41 +0100

    heroku master config 1

--- orbital_decay/settings.py
+++ orbital_decay/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,4 +123,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-django_heroku.settings(locals())
